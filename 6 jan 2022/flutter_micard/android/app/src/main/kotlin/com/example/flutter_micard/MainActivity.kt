b9b30a9b792a1335189ac926f6725a2542f43d61
Rakibuldroid <rakibul66h@gmail.com>
2022-01-27 01:19:16 +0600

    trip cost app
    trip cost app and speech to text app

--- 6 jan 2022/flutter_micard/android/app/src/main/kotlin/com/example/flutter_micard/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_micard
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ speechtext/android/app/src/main/kotlin/com/example/uipractise/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.uipractise
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
